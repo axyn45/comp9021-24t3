5a907119dff025b1318847cbe259460301b6f03f
axyn45 <axyn45@outlook.com>
2024-10-04 12:55:40 +1000

    game()

--- ass1/solitaire_1.py
+++ ass1/solitaire_1.py
@@ -2,5 +2,15 @@ from itertools import chain
 from random import seed, shuffle
 from collections import defaultdict
 
+
+def game():
+    # def play():
+
+    seed=input('Please enter an integer to feed the seed() function: ')
+    deck=shuffle(list(range(52)))
+    print('\nDeck shuffled, ready to start!\U0001F0A8')
+    print(']'*52)
+    print('\nStarting first round...')
 # INSERT YOUR CODE HERE
 
+game()
